c6b590987d7c929866f9e8eeea02d426ff9957f5
aghie <david.vilares89@gmail.com>
2019-09-16 12:56:11 +0200

    Added option to indicate .prm file

--- scripts/run_ncrfpp_multitask_ctb.sh
+++ scripts/run_ncrfpp_multitask_ctb.sh
@@ -9,6 +9,7 @@ INPUT=../sample_data/cp_datasets/ctb/ctb-$TEST_NAME.seq_lu
 TEST_PATH=../sample_data/cp_datasets/CTB_pred_tags/$TEST_NAME"_ch.trees"
 USE_GPU=False
 EVALB=../EVALB/evalb
+EVALB_PARAM=../EVALB/COLLINS.prm
 OUTPUT=../outputs/
 MODELS=../pretrained_models_naacl2019/ctb/
 NCRFPP=$HOME_NCRFpp
@@ -29,7 +30,8 @@ python ../run_ncrfpp.py \
 --gpu $USE_GPU \
 --output $OUTPUT/ctb.emnlp2018.f.$TEST_NAME.$USE_GPU.txt \
 --evalb $EVALB \
---ncrfpp $NCRFPP #> $LOGS/ctb.emnlp2018.f.$TEST_NAME.$USE_GPU.log 2>&1
+--evalb_param $EVALB_PARAM \
+--ncrfpp $NCRFPP > $LOGS/ctb.emnlp2018.f.$TEST_NAME.$USE_GPU.log 2>&1
 
 
 taskset --cpu-list 1 \
@@ -41,7 +43,8 @@ python ../run_ncrfpp.py \
 --gpu $USE_GPU \
 --output $OUTPUT/ctb.emnlp2018.$TEST_NAME.$USE_GPU.txt \
 --evalb $EVALB \
---ncrfpp $NCRFPP #> $LOGS/ctb.emnlp2018.f.3R.-2.$TEST_NAME.$USE_GPU.log 2>&1
+--evalb_param $EVALB_PARAM \
+--ncrfpp $NCRFPP > $LOGS/ctb.emnlp2018.f.3R.-2.$TEST_NAME.$USE_GPU.log 2>&1
 
 
 ###################################################
@@ -59,7 +62,8 @@ python ../run_ncrfpp.py \
 --multitask \
 --output $OUTPUT/ctb.multitask.$TEST_NAME.$USE_GPU.txt \
 --evalb $EVALB \
---ncrfpp $NCRFPP #> $LOGS/ctb.multitask.3R.-2.$TEST_NAME.$USE_GPU.log 2>&1
+--evalb_param $EVALB_PARAM \
+--ncrfpp $NCRFPP > $LOGS/ctb.multitask.3R.-2.$TEST_NAME.$USE_GPU.log 2>&1
 
 
 
@@ -79,7 +83,8 @@ python ../run_ncrfpp.py \
 --multitask \
 --output $OUTPUT/ctb.multitask.$TEST_NAME.$USE_GPU.txt \
 --evalb $EVALB \
---ncrfpp $NCRFPP #> $LOGS/ctb.multitask.3R.-2.nex_lev.$TEST_NAME.$USE_GPU.log 2>&1 
+--evalb_param $EVALB_PARAM \
+--ncrfpp $NCRFPP > $LOGS/ctb.multitask.3R.-2.nex_lev.$TEST_NAME.$USE_GPU.log 2>&1 
 
 
 taskset --cpu-list 1 \
@@ -92,7 +97,8 @@ python ../run_ncrfpp.py \
 --multitask \
 --output $OUTPUT/ctb.RL.multitask.$TEST_NAME.$USE_GPU.txt \
 --evalb $EVALB \
---ncrfpp $NCRFPP #> $LOGS/ctb.multitask.3R.-2.nex_lev.$TEST_NAME.$USE_GPU.log 2>&1 
+--evalb_param $EVALB_PARAM \
+--ncrfpp $NCRFPP > $LOGS/ctb.RL.multitask.3R.-2.nex_lev.$TEST_NAME.$USE_GPU.log 2>&1 
 
 
 
